87665b43ac186d15726d72c2a1639179122bc057
unknown <ubaid>
2025-10-08 11:30:49 +0530

    v5

--- main.tf
+++ main.tf
@@ -1,55 +1,55 @@
-# module "iam" {
-#   source = "./module/iam"
-# }
-# module "SG"{
-#   source = "./module/security_groups"
-#   vpc_id = module.vpc.vpcid
-# }
+module "iam" {
+  source = "./module/iam"
+}
+module "SG"{
+  source = "./module/security_groups"
+  vpc_id = module.vpc.vpcid
+}
 
-# module "vpc" {
-#   source = "./module/vpc"
-# }
-
-
-# module "ec2_1" {
-#   source = "./module/ec2"
-#   Name   = "${var.Name}-dev"
-#   SG     = module.SG.aws_security_group_id
-#   instance_profile = module.iam.instance_profile_name
-#   subnet_id        = module.vpc.pubsub1
-# }
-
-# module "ec2_2" {
-#   source = "./module/ec2"
-#   Name   = "${var.Name}-stage"
-#   SG     = module.SG.aws_security_group_id
-#   instance_profile = module.iam.instance_profile_name
-#   subnet_id        = module.vpc.pubsub2
-# }
-
-# module "ec2_3" {
-#   source = "./module/ec2"
-#   Name   = "${var.Name}-prod"
-#   SG     = module.SG.aws_security_group_id
-#   instance_profile = module.iam.instance_profile_name
-#   subnet_id        = module.vpc.pubsub1
-# }
+module "vpc" {
+  source = "./module/vpc"
+}
 
 
+module "ec2_1" {
+  source = "./module/ec2"
+  Name   = "${var.Name}-dev"
+  SG     = module.SG.aws_security_group_id
+  instance_profile = module.iam.instance_profile_name
+  subnet_id        = module.vpc.pubsub1
+}
+
+module "ec2_2" {
+  source = "./module/ec2"
+  Name   = "${var.Name}-stage"
+  SG     = module.SG.aws_security_group_id
+  instance_profile = module.iam.instance_profile_name
+  subnet_id        = module.vpc.pubsub2
+}
+
+module "ec2_3" {
+  source = "./module/ec2"
+  Name   = "${var.Name}-prod"
+  SG     = module.SG.aws_security_group_id
+  instance_profile = module.iam.instance_profile_name
+  subnet_id        = module.vpc.pubsub1
+}
 
 
-# module "alb" {
-#   source  = "./module/load_balancer"
 
-#   vpc_id  = module.vpc.vpcid
 
-#   subnets = [
-#     module.vpc.pubsub1,
-#     module.vpc.pubsub2
-#   ]
+module "alb" {
+  source  = "./module/load_balancer"
 
-#   SG       = module.SG.aws_security_group_id
-#   ec2_1_id = module.ec2_1.ec2_id
-#   ec2_2_id = module.ec2_2.ec2_id
-#   ec2_3_id = module.ec2_3.ec2_id
-# }
+  vpc_id  = module.vpc.vpcid
+
+  subnets = [
+    module.vpc.pubsub1,
+    module.vpc.pubsub2
+  ]
+
+  SG       = module.SG.aws_security_group_id
+  ec2_1_id = module.ec2_1.ec2_id
+  ec2_2_id = module.ec2_2.ec2_id
+  ec2_3_id = module.ec2_3.ec2_id
+}
