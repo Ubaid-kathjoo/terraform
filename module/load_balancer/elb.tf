ad56b4013246560e63ad36a2b6ed793dc500b973
unknown <ubaid>
2025-10-24 10:33:59 +0530

    v13

--- module/load_balancer/elb.tf
+++ module/load_balancer/elb.tf
@@ -24,21 +24,18 @@ resource "aws_lb_target_group" "this" {
 }
 
 resource "aws_lb_target_group_attachment" "ec2_1" {
-  count            = var.env == "dev" 
   target_group_arn = aws_lb_target_group.this.arn
   target_id        = var.ec2_1_id[0]
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "ec2_2" {
-  count            = var.env == "stage" 
   target_group_arn = aws_lb_target_group.this.arn
   target_id        = var.ec2_2_id[0]
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "ec2_3" {
-  count            = var.env == "prod"
   target_group_arn = aws_lb_target_group.this.arn
   target_id        = var.ec2_3_id[0]
   port             = 80
