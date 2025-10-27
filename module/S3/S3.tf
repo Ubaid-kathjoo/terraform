f1d338c2cbbe79881b37c0e27faa67cc1dd741b9
unknown <ubaid>
2025-10-27 15:54:54 +0530

    v23

--- module/S3/S3.tf
+++ module/S3/S3.tf
@@ -1,16 +1,4 @@
 resource "aws_s3_bucket" "env_bucket" {
-  bucket = "${var.my_project}-" 
+  bucket = "${var.my_project}-name" 
 
-  tags = {
-    Name        = "${var.my_project}"
-    Environment = var.env
-  }
-}
-resource "aws_s3_bucket" "this" {
-  bucket = "${var.my_project}-${var.env}"
-
-  tags = {
-    Name        = "${var.my_project}-${var.env}-bucket"
-    Environment = var.env
-  }
 }
