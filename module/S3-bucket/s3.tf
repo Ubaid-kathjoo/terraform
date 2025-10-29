5d83f06c20cbf0f87eb980ca0ac1ae9a7ac69a36
unknown <ubaid>
2025-10-29 13:45:17 +0530

    v26

--- module/S3-bucket/s3.tf
+++ module/S3-bucket/s3.tf
@@ -12,9 +12,9 @@ resource "random_id" "suffix" {
 resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.env_bucket.id
 
-  policy = templatefile("${path.module}/policies/s3_bucket_policy.json.tpl", {
-  bucket_name = aws_s3_bucket.env_bucket.id
-  env         = var.env
+policy = templatefile("${path.module}/policies/s3_bucket_policy.json.tpl", {
+  bucket_name = aws_s3_bucket.env_bucket.id,
+  env         = var.env,
   account_id  = data.aws_caller_identity.current.account_id
   })
 }
