c76e4e71357da7e27bd6f48c1e5745532128a126
unknown <ubaid>
2025-10-30 16:03:39 +0530

    s3 iam

--- main.tf
+++ main.tf
@@ -1,44 +1,44 @@
-module "s3_dev" {
-  count = var.env == "dev" ? 1 : 0
-  source = "./module/S3-bucket"
-  my_project = "myproject"
-  env = var.env
-}
+# module "s3_dev" {
+#   count = var.env == "dev" ? 1 : 0
+#   source = "./module/S3-bucket"
+#   my_project = "myproject"
+#   env = var.env
+# }
 
-module "s3_stage" {
-  count = var.env == "stage" ? 1 : 0
-  source       = "./module/S3-bucket"
-  my_project  = "myproject"
-  env          = var.env
-}
+# module "s3_stage" {
+#   count = var.env == "stage" ? 1 : 0
+#   source       = "./module/S3-bucket"
+#   my_project  = "myproject"
+#   env          = var.env
+# }
 
-module "s3_prod" {
-  count = var.env == "prod" ? 1 : 0
-  source       = "./module/S3-bucket"
-  my_project = "myproject"
-  env          = var.env
-}
+# module "s3_prod" {
+#   count = var.env == "prod" ? 1 : 0
+#   source       = "./module/S3-bucket"
+#   my_project = "myproject"
+#   env          = var.env
+# }
 
-module "iam_test" {
-  count = var.env == "dev" ? 1 : 0
-  source     = "./module/iam-role"
-  env        = "test"
-  s3_actions = ["s3:*"]
-  role_name   = "test-role"
-}
+# module "iam_test" {
+#   count = var.env == "dev" ? 1 : 0
+#   source     = "./module/iam-role"
+#   env        = "test"
+#   s3_actions = ["s3:*"]
+#   role_name   = "test-role"
+# }
 
-module "iam_stage" {
-  count = var.env == "stage" ? 1 : 0
-  source     = "./module/iam-role"
-  env        = "stage"
-  s3_actions = ["s3:GetObject", "s3:PutObject"]  
-  role_name   = "stage-role"
-  }
+# module "iam_stage" {
+#   count = var.env == "stage" ? 1 : 0
+#   source     = "./module/iam-role"
+#   env        = "stage"
+#   s3_actions = ["s3:GetObject", "s3:PutObject"]  
+#   role_name   = "stage-role"
+#   }
 
-module "iam_prod" {
-  count = var.env == "prod" ? 1 : 0
-  source     = "./module/iam-role"
-  env        = "prod"
-  s3_actions = ["s3:GetObject", "s3:ListBucket"]
-  role_name   = "prod-role"
-}
+# module "iam_prod" {
+#   count = var.env == "prod" ? 1 : 0
+#   source     = "./module/iam-role"
+#   env        = "prod"
+#   s3_actions = ["s3:GetObject", "s3:ListBucket"]
+#   role_name   = "prod-role"
+# }
