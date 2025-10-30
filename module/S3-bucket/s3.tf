f7dcb1c795f5ab928a531cc4bd79da82f8961e94
unknown <ubaid>
2025-10-30 15:56:26 +0530

    s3 iam

--- module/S3-bucket/s3.tf
+++ module/S3-bucket/s3.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.env_bucket.id
 
 policy = templatefile("${path.module}/policies/s3_bucket_policy.json.tpl", {
-  bucket_name = aws_s3_bucket.env_bucket.name
+  bucket_name = aws_s3_bucket.env_bucket.id
   env         = var.env,
   account_id  = data.aws_caller_identity.current.account_id
   })
