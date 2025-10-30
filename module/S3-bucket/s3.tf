b3e8d300d31dd4e767034aed7774f4bda0c11f2c
unknown <ubaid>
2025-10-30 15:54:33 +0530

    s3 iam

--- module/S3-bucket/s3.tf
+++ module/S3-bucket/s3.tf
@@ -1,19 +1,17 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "env_bucket" {
-  bucket = "${var.my_project}-${var.env}-bucket-${random_id.suffix.hex}"
+  bucket = "${var.my_project}-${var.env}-bucket-ubaid12345-s3"
 }
 
-resource "random_id" "suffix" {
-  byte_length = 2
-}
+
 
 
 resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.env_bucket.id
 
 policy = templatefile("${path.module}/policies/s3_bucket_policy.json.tpl", {
-  bucket_name = aws_s3_bucket.env_bucket.id,
+  bucket_name = aws_s3_bucket.env_bucket.name
   env         = var.env,
   account_id  = data.aws_caller_identity.current.account_id
   })
