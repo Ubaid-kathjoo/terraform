ca1830c141d908aa091eae0f6f397af027da20aa
ubaid <ubaid@gmail.com>
2026-04-17 16:46:07 +0530

    v6

--- module/S3-bucket/s3.tf
+++ module/S3-bucket/s3.tf
@@ -1,16 +1,13 @@
-resource "aws_s3_bucket" "env_bucket" {
-  bucket = var.bucket_name
-}
-
-
-
-
 resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.env_bucket.id
 
-policy = templatefile("${path.module}/policies/s3_bucket_policy.json.tpl", {
-  account_id  = var.account_id
-  env         = var.env
-  bucket_name = var.bucket_name
-})
+  policy = jsonencode(
+    jsondecode(
+      templatefile("${path.module}/policies/s3_bucket_policy.json.tpl", {
+        account_id  = var.account_id
+        env         = var.env
+        bucket_name = var.bucket_name
+      })
+    )
+  )
 }
