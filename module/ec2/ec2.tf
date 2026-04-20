d51e1db318d04c7ef54eb972e1b4a1fa00c2acdf
ubaid <ubaid@gmail.com>
2026-04-20 13:13:49 +0530

    appache server and sg

--- module/ec2/ec2.tf
+++ module/ec2/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "this" {
   ami           = data.aws_ami.latest.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
