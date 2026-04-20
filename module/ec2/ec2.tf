eb356cb9153d1f2338c1fe679232d1c3c9c2f820
ubaid <ubaid@gmail.com>
2026-04-20 13:08:04 +0530

    appache server

--- module/ec2/ec2.tf
+++ module/ec2/ec2.tf
@@ -1,6 +1,14 @@
 resource "aws_instance" "this" {
   ami           = data.aws_ami.latest.id
   instance_type = var.instance_type
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install httpd -y
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Hello from ${var.env} server</h1>" > /var/www/html/index.html
+              EOF
 
   tags = {
     Name = "${var.env}-instance"
